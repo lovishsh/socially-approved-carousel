76e83a0f1d0edc754db7132be0921d1cf370d71e
Lovish Jha <lovish.jha@comfygentechnologies.com>
2026-06-27 11:15:05 +0530

    changes in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
+    allowedHosts: ["socially-approved-carousel-1.onrender.com"],
+  },
+  preview: {
+    allowedHosts: ["socially-approved-carousel-1.onrender.com"],
   },
 });
